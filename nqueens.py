dca57d109e8cee95f0b5b7831ae68e948bb4e74b
adrianvuong <60362045+adrianvuong@users.noreply.github.com>
2022-11-30 16:14:48 -0500

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -1,19 +1,20 @@
 import random
 
 def randomQueens(board, n):
-    randList = random.sample(range(1,n),n)
+    randList = random.sample(range(1,n*n),n)
  
-    
     return randList
     
 def minConflicts(csp, max_steps, current):
-
-    return None
     
-def main():    
+    current = None
+    for i in range(max_steps):
+        
+        return None
+
+def __main__():    
     choice = input("Choose value of n:\n1.10\n2.100\n3.1000\n4.10000\n5.100000\n6.10000000\n")
     n = 10;
-    board
     match choice:
         case "1":
             n = 10
@@ -29,17 +30,23 @@ def main():
             n = n*n*n*n*n*n
         case _:
             print("Invalid value")
+    
     board = [[0 for x in range(n)] for y in range(n)]
+ 
     list = randomQueens(0,n)
-    for i in range(n):
-        for j in range(n):
-            if(list[i] == i + j):
-                board[i][j] = 1
+    positions = []
+    for i in range(1, len(list)):
+        board[list[i] // n][list[i]%n] = 1 
         
     
-    #print(list)
 
-main()
+    print(list)
+    for i in range(n):
+        print(board[i],"\n")
+    
+    
+    
+    #print(list)
 class Queen:
     
     def __init__(self, board):
@@ -47,3 +54,4 @@ class Queen:
         self.constraints = list()
         return None
 
+__main__()
