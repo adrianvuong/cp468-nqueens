803f8e1ea8e4e7d88e7017aa981b3f71d3078703
adrianvuong <adrianoz5545@gmail.com>
2022-12-05 20:54:30 -0500

    fixed

---
+++ main.py
@@ -0,0 +1,76 @@
+import sys, time
+
+from copy import deepcopy
+from nqueens import Board, CSP
+from nqueens import *
+from random import choice
+
+#option = input("Choose value of n:\n1.10\n2.100\n3.1000\n4.10000\n5.100000\n6.10000000\n")
+n = int(input("choose value of n\n"))
+# match option:
+#     case "1":
+#         n = 10
+#     case "2":
+#         n = n*n
+#     case "3":
+#         n = n*n*n
+#     case "4":
+#         n = n*n*n*n
+#     case "5":
+#         n = n*n*n*n*n
+#     case "6":
+#         n = n*n*n*n*n*n
+#     case _:
+#         print("Invalid value")
+
+start_time = time.time()
+board = Board(n)
+variables = [i for i in range(1, n+1)]
+_var = deepcopy(variables)
+constraints = {i: 0 for i in variables}
+y = choice(_var)
+domains = {1: y}
+_var.remove(y)
+board.set_queen(1, y, constraints)
+
+
+for i in range(2, n+1):
+    y = get_least_conflicts_y(i, n, _var, board)
+    domains[i] = y
+    board.set_queen(i, y, constraints)
+    _var.remove(y)
+
+csp = CSP(variables, domains, constraints)
+
+if(n <= 100):
+    print('\nInitial Board')
+    b = create_board(n)
+    for key, value in csp.domains.items():
+        if constraints[key] > 0:
+            b[value-1][key -1] = 'Q' 
+        else:
+            b[value - 1][key - 1] = 'Q' 
+
+    print_board(b)
+
+solve_time = time.time()
+assignment = min_conflicts(csp, n, board)
+
+if assignment:
+    end_time = time.time()
+    print('Solve Time: {:0.5f} secs'.format(end_time - solve_time))
+
+    if(n <= 100):
+        print('\nSolved')
+        b = create_board(n)
+        for key, value in assignment.domains.items():
+            b[value - 1][key - 1] ='Q' 
+
+        print_board(b)
+
+        #print("Board too big, printing to file")
+        with open("output.txt", "w") as f:
+            for i in csp.domains:
+                print(i,csp.domains[i], file = f, end = "\n")
+else:
+    print('Increase Max Steps to solve.')
--- nqueens.py
+++ nqueens.py
@@ -1,57 +1,143 @@
-import random
+from random import choice
 
-def randomQueens(board, n):
-    randList = random.sample(range(1,n*n),n)
- 
-    return randList
-    
-def minConflicts(csp, max_steps, current):
-    
-    current = None
-    for i in range(max_steps):
-        
-        return None
+class Board:
 
-def __main__():    
-    choice = input("Choose value of n:\n1.10\n2.100\n3.1000\n4.10000\n5.100000\n6.10000000\n")
-    n = 10;
-    match choice:
-        case "1":
-            n = 10
-        case "2":
-            n = n*n
-        case "3":
-            n = n*n*n
-        case "4":
-            n = n*n*n*n
-        case "5":
-            n = n*n*n*n*n
-        case "6":
-            n = n*n*n*n*n*n
-        case _:
-            print("Invalid value")
-    
-    board = [[0 for x in range(n)] for y in range(n)]
- 
-    list = randomQueens(0,n)
-    positions = []
-    for i in range(1, len(list)):
-        board[list[i] // n][list[i]%n] = 1 
-        
-    
+    def __init__(self, n):
+        self._n = n
+        self._queen_rows = {i: set() for i in range(1, self._n+1)}
+        self._queen_posdiag = {i: set() for i in range(1, 2 * self._n)}
+        self._queen_negdiag = {i: set() for i in range(1, 2 * self._n)}
 
-    print(list)
-    for i in range(n):
-        print(board[i],"\n")
-    
-    
-    
-    #print(list)
-class Queen:
-    
-    def __init__(self, board):
-        self.variables = list()
-        self.constraints = list()
-        return None
+    def set_queen(self, x, y, constraints):
 
-__main__()
+        combined = self._queen_rows[y] | self._queen_posdiag[y+(x-1)] | self._queen_negdiag[y + (self._n - x)]
+
+        for i in combined:
+            constraints[i] += 1
+
+        self._queen_rows[y].add(x)
+        self._queen_posdiag[y+(x-1)].add(x)
+        self._queen_negdiag[y+(self._n - x)].add(x)
+
+        constraints[x] = len(combined)
+        return
+
+    def remove_queen(self, x, y, constraints):
+
+        combined = self._queen_rows[y] | self._queen_posdiag[y+(x-1)] | self._queen_negdiag[y + (self._n - x)]
+
+        for i in combined:
+            constraints[i] -= 1
+
+        self._queen_rows[y].remove(x)
+        self._queen_posdiag[y+(x-1)].remove(x)
+        self._queen_negdiag[y+(self._n - x)].remove(x)
+
+        constraints[x] = 0
+        return
+
+    def get_num_conflicts(self, x, y):
+
+        combined = self._queen_rows[y] | self._queen_posdiag[y+(x-1)] | self._queen_negdiag[y + (self._n - x)]
+
+        return len(combined)
+
+
+class CSP:
+
+    def __init__(self, variables , domains, constraints):
+        self.variables = variables
+        self.domains = domains
+        self.constraints = constraints
+
+
+def min_conflicts(csp, n, board, max_steps=100):
+
+    past_var = {}
+    past_queen = None
+
+    x = 50 if n >= 100 else (n//2)
+
+    for i in range(1, max_steps+1):
+        conflicted = [i for i, j in csp.constraints.items() if j != 0]
+
+        if conflicted == []:
+            print('Steps: {}'.format(i))
+            return csp
+
+        if past_queen is not None and past_queen in conflicted:
+            conflicted.remove(past_queen)
+
+        var = choice(conflicted)
+        past_queen = var
+        board.remove_queen(var, csp.domains[var], csp.constraints)
+
+        if var in past_var:
+            if csp.domains[var] not in past_var[var]:
+                past_var[var].append(csp.domains[var])
+        else:
+            past_var[var] = [csp.domains[var]]
+
+        value = conflicts(var, csp.domains[var], n, csp, past_var[var], board)
+        if len(past_var[var]) >= x: past_var[var].pop(0)
+
+        csp.domains[var] = value
+        board.set_queen(var, value, csp.constraints)
+    return False
+
+
+def get_least_conflicts_y(x, n, possible, board):
+
+    conflict_list, min_count = [possible[0]], board.get_num_conflicts(x, possible[0])
+
+    for i in possible[1:]:
+        count = board.get_num_conflicts(x, i)
+        if min_count > count:
+            min_count = count
+            conflict_list = [i]
+        elif min_count == count:
+            conflict_list.append(i)
+
+    return choice(conflict_list)
+
+
+def conflicts(var, v, n, csp, not_possible, board):
+
+    x, y = var, v
+    conflict_list, min_count = [], None
+
+    for i in range(1, n+1):
+        if i == y: continue
+        count = board.get_num_conflicts(x, i)
+        if min_count is not None and min_count > count:
+            min_count = count
+            conflict_list = [i]
+        elif min_count is not None and min_count == count:
+            conflict_list.append(i)
+        elif min_count is None:
+            min_count = count
+            conflict_list = [i]
+
+    clist = list(set(conflict_list) - set(not_possible))
+    if clist != []:
+        return choice(clist)
+
+    return choice(conflict_list)
+
+def create_board(n):
+    return [['-']*n for i in range(n)]
+
+def print_board(board):
+    for x in board:
+        for y in x:
+            print(y, end=' ')
+        print()
+    return
+
+def print_boardtofile(board):
+    with open("output.txt", "w") as f:
+        for x in board:
+            for y in x:
+                f.write(y)
+        f.write("\n")
+    return
